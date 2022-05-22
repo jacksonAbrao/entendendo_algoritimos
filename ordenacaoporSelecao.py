51d45eaae4a8ab28bf0207c01137887ebf10a967
jacksonAbrao <jackson.comp1681@hotmail.com>
2022-05-21 22:48:45 -0300

    alt

--- ordenacaoporSelecao.py
+++ ordenacaoporSelecao.py
@@ -1,7 +1,7 @@
 def buscaMenor(arr):
     menor = arr[0]
     menor_indice = 0
-    for i in range(1, len(arr)): #range vai fazer uma sequência
+    for i in range(1, len(arr)): #range vai fazer uma sequência onde o primeiro elemento é o 1, definido ali, e o ultimo é o tamanho do array
         if arr[i] < menor :
             menor = arr[i]
             menor_indice = i
@@ -11,7 +11,7 @@ def ordenacaoporSelecao(arr):
     novoArr = []
     for i in range(len(arr)):
         menor = buscaMenor(arr)
-        novoArr.append(arr.pop(menor))
+        novoArr.append(arr.pop(menor)) #pop retira o item do array e retora ele e o append inclui ele no novo array
     return novoArr
 
 print(ordenacaoporSelecao([5, 3, 6, 2, 10, 25, 1, -3, 0, 44]))
